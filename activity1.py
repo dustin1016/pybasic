5177bbbab932fb4f55f8f1fefea01ed9892a161c
dustin1016 <dev.dustindale@gmail.com>
2024-09-25 08:11:46 +0800

    .

--- activity1.py
+++ activity1.py
@@ -1,4 +1,14 @@
+import getdatatype
+def eval_bool(param):
+    if isinstance(param, bool):
+        return True
+    else:
+        return False
+
 x = "hello"
 
-print(type(x))
-print(isinstance(x, str))
+# print(type(x))
+# print(isinstance(x, int))
+b = True
+f = str(b)
+print(eval_bool(b))
